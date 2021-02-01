fb9d8911c618175a0215c362013dbd15a81ec6cd
junho.yoon@kt.com <junho.yoon@kt.com>
2021-02-01 15:22:23 +0900

    submain commit test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 public class App extends SpringBootServletInitalizer {
     public String sayHello() {
         System.out.println("세번째 test");
+        System.out.println("submain test");
         return "Hello world!";
 
     }
